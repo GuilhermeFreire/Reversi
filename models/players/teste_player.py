dd6164edb6382af26a0a96a6ccced7aaf488ffcf
Mateus Ildefonso <matecompufrj@gmail.com>
2016-12-12 00:00:20 -0200

    ADD quiescence

--- models/players/teste_player.py
+++ models/players/teste_player.py
@@ -4,14 +4,14 @@ from models.state import State
 
 
 
-class MinMax:
+class Teste:
 	EMPTY, BLACK, WHITE, OUTER = '.', '□', '■', '?'
 
 	#Construtor precisa apenas da cor do player e o nivel de profundidade
-	def __init__(self, color, level=4, threshold=30):
+	def __init__(self, color, level=4):
 		self.color = color
 		self.level = level
-		self.threshold = threshold
+		self.last = 2
 		self.pos_values = [[20, -3, 11, 8, 8, 11, -3, 20],
     						[-3, -7, -4, 1, 1, -4, -7, -3],
     						[11, -4, 2, 2, 2, 2, -4, 11],
@@ -22,30 +22,12 @@ class MinMax:
     						[20, -3, 11, 8, 8, 11, -3, 20]]
 
 	#Constroi a arvore a partir do estado atual do tabuleiro
-	def build_tree(self, state, temp_level, color, cut=None, father=None):
+	def build_tree(self, state, temp_level, color, cut=None):
 		#Caso base, nivel_atual = nivel_maximo
 		if temp_level == self.level:
 			scores = self.score(state.get_board())
 			# Score retorna pontuacao [white, black]
-			score = scores[0] if self.color == MinMax.WHITE else scores[1]
-			if abs(score - father) > self.threshold:
-				print "Entrei:", score, father
-				state.make_children(color)
-				children = state.get_children()
-				if children == []:
-					children.append(state)
-
-				scores = []
-				for child in children:
-					child_score = self.score(state.get_board())
-					score = scores[0] if self.color == MinMax.WHITE else scores[1]
-					scores.append(score)
-
-				if color == self.color:
-					score = max(scores)
-				else:
-					score = min(scores)
-
+			score = scores[0] if self.color == Teste.WHITE else scores[1]
 			# print "Final level:", score, scores
 			del state
 			return score, -1
@@ -76,26 +58,23 @@ class MinMax:
 		#e alterando a cor do estado.
 		First = True
 		ab = None
-		if temp_level == self.level -1:
-			father = self.score(state.get_board())
-			# Score retorna pontuacao [white, black]
-			father = father[0] if color == MinMax.WHITE else father[1]
-
 		for child in children:
 			if First == True:
 				# print "Chamando primeiro filho"
-				score, _ = self.build_tree(child,temp_level+1,opponent_color,None,father)
+				score, _ = self.build_tree(child,temp_level+1,opponent_color,None)
 				scores.append(score)
 				ab = score
 				First = False
 			else:
 				# print "Chamando n-esimo filho"
 				ab = max(scores) if self.color == color else min(scores)
-				score, _ = self.build_tree(child,temp_level+1,opponent_color,ab,father)
+				score, _ = self.build_tree(child,temp_level+1,opponent_color,ab)
 				scores.append(score)
+
 			# Se o pai nao tem seu valor para realizar o corte
 			# Faca o Minmax normalmente...
 			if cut is None: continue
+
 			# Caso ele tenha, significa que um filho ja foi calculado
 			# Logo podemos verificar se e possivel descartar este estado
 			if self.color == color:
@@ -117,7 +96,6 @@ class MinMax:
 		#O jogador que chamou o MINMAX quer obter o maior valor possivel
 		#enquanto o oponente quer diminuir a pontuacao do jogador.
 		# print color, self.color
-
 		if color == self.color:
 			idx = np.argmax(scores)
 		else:
@@ -143,7 +121,7 @@ class MinMax:
 	#objeto board, entao se usarmos o is... da ruim
 	#board.WHITE != minmax.WHITE
 	def _opponent(self, color):
-		return MinMax.BLACK if color == MinMax.WHITE else MinMax.WHITE
+		return Teste.BLACK if color == Teste.WHITE else Teste.WHITE
 
 	#Calcula a pontuacao de um stado
 	def score(self, board):
@@ -151,33 +129,12 @@ class MinMax:
 		black = 0
 		for i in range(1, 9):
 			for j in range(1, 9):
-				if board.get_square_color(i, j) == MinMax.WHITE:
+				if board.get_square_color(i, j) == Teste.WHITE:
 					white += self.pos_values[i - 1][j - 1]
-				elif board.get_square_color(i, j) == MinMax.BLACK:
+				elif board.get_square_color(i, j) == Teste.BLACK:
 					black += self.pos_values[i - 1][j - 1]
 
 		return [white, black]
-
-	# def quiescence(self,alpha,beta,score, level):
-	# 	if level == 3:
-	# 		return score
-
-	# 	if score >= beta:
-	# 		return beta
-	# 	if score > alpha:
-	# 		alpha = score
-
-	# 	while (verify_all_captures):
-	# 		MakeCapture()
-	# 		score = -quiescence(-beta, -alpha, score, max_level)
-	# 		TakeBackMove();
-
-	# 		if score >= beta:
-	# 			return beta;
-	# 		if score > alpha:
-	# 			alpha = score;
-
-	# 	return alpha;
 
 	#Faz jogada.
 	def play(self, board):
@@ -185,5 +142,12 @@ class MinMax:
 		root = State(board)
 		#Apois contruir a arvore e obtemos o indice do array de movimentos
 		#que corresponde a jogada que o minmax decidiu.
-		_, idx_move = self.build_tree(root,1,self.color,None,None)
+		tiles = sum(root.get_board().score())
+		# print tiles, self.level
+		if tiles > 25:
+			self.level = 5
+		if tiles > 55:
+			self.level = 8
+
+		_, idx_move = self.build_tree(root,1,self.color,None)
 		return root.board.valid_moves(self.color)[idx_move]
